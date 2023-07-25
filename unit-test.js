a4ee61f7568f2825429b46cc5b56d50403161184
trevornei <117675657+trevornei@users.noreply.github.com>
2023-07-24 18:58:01 -0600

    updated comments in unit-test.js

--- unit-test.js
+++ unit-test.js
@@ -1,6 +1,9 @@
 // IMPORTANT PLEASE READ!!
 /* To run the unit test without running into the error go to ----> deck.js line 23.
    NOTE: delete 'export default' from line 23 to run unit test error.  
+
+   Mocha and Chai, being dependent on Node.js, run into errors with import, export, and default statements. 
+   Node.js Does not support the above features of ES6 because Node.js uses common.js
    
    Error: "Unexpected token 'export' (at deck.js:23:1)"
 */
